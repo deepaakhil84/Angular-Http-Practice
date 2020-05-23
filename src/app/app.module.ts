ade0e88bd3e03a71c3744cfeb7877622dcc9ee3b
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-05-23 14:25:00 +0100

    created a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListUserComponent } from './users/list-user/list-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListUserComponent
   ],
   imports: [
     BrowserModule
